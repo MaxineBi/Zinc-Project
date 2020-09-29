b4314b0d9dafb27d7fcf5220aefa269329c60d62
simoneaharrison <70298985+simoneaharrison@users.noreply.github.com>
2020-09-29 14:16:25 -0700

    Update Final1.py

--- Final1.py
+++ Final1.py
@@ -21,3 +21,36 @@ for filename in glob.glob(os.path.join(folder_path, '*.ent')):
    ppb = PPBuilder()
    for pp in ppb.build_peptides(read_input2(filename)):
     print(pp.get_sequence())
+   
+ SIMONE's CODE:
+ def molecular_weight_printer(filename):
+ ppb = PPBuilder()
+ MW_final = 0
+ print(filename[25:29])
+ for pp in ppb.build_peptides(structure(filename)):
+  seq = pp.get_sequence()
+  seqstring = str(seq)
+  analysed_seq = ProteinAnalysis(seqstring)
+  MW = analysed_seq.molecular_weight()
+  MW_final += MW
+ print(MW_final)
+
+def sequence_printer(filename):
+ ppb = PPBuilder()
+ print('>',filename[25:29])
+ for pp in ppb.build_peptides(structure(filename)):
+  seq = pp.get_sequence()
+  seqstring = str(seq)
+  print(seqstring)
+
+metalsite =  'BINDING SITE FOR RESIDUE ZN'
+metalsitelower = 'binding site for residue ZN'
+conditions = 'CRYSTALLIZATION CONDTIONS: ...  ZINC'
+
+
+for filename in glob.glob(os.path.join(folder_path, '*.ent')):
+  with open(filename, 'r') as f:
+   FileContents = f.read()
+   if FileContents.find(metal) != -1 and FileContents.find(filetype) != -1 and FileContents.find(conditions) != -1:
+    molecular_weight_printer(filename)
+                                      
