d6f38ef5f6bc44567eaa6e7dd5b0368ce11dcc87
simoneaharrison <70298985+simoneaharrison@users.noreply.github.com>
2020-09-30 09:36:14 -0700

    Update PDBsequencereader.py

--- pdb database sequence and MW program/PDBsequencereader.py
+++ pdb database sequence and MW program/PDBsequencereader.py
@@ -31,4 +31,4 @@ for filename in glob.glob(os.path.join(folder_path, '*.ent')):
   with open(filename, 'r') as f:
    FileContents = f.read()
    if FileContents.find(metal) != -1 and FileContents.find(filetype) != -1:
-     print(sequencefilename))
+     print(sequence(filename))
